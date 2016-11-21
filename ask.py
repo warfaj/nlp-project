5a39c02faeec95ecc9eb2967741cccb905c48955
xiaowilliam <williamx@andrew.cmu.edu>
2016-11-21 00:30:30 -0500

    Add files via upload

--- ask.py
+++ ask.py
@@ -1,40 +1,8 @@
 import sys
-import os
 import en
 import re
 
-from nltk.parse import stanford
-from nltk import word_tokenize, pos_tag
-from nltk.tag.stanford import StanfordNERTagger
-from nltk.tree import Tree
-
-#dir = '/Users/warfajibril/PycharmProjects/NLPProject/'
-#dir = '~/Desktop/nlp project/nlp-project-master/'
-dir = ''
-
-class Sentence(object):
-
-    def __init__(self, raw_sentence, id):
-        self.id = id
-        self.raw_text=raw_sentence
-        self.tokenized_text=word_tokenize(raw_sentence)
-        self.parser = self.get_parser()
-        self.parse_tree= self.parser.raw_parse(raw_sentence)
-        self.pos_tags = pos_tag(self.tokenized_text)
-        self.ner_tags = self.get_ner_tags()
-
-
-
-    def get_parser(self):
-        os.environ['CLASSPATH'] = dir+'stanford-parser'
-        os.environ['STANFORD_PARSER'] = dir+'stanford-parser/stanford-parser.jar'
-        os.environ['STANFORD_MODELS'] = dir+'stanford-parser/stanford-parser-3.6.0-models.jar'
-        parser = stanford.StanfordParser(model_path=dir+"stanford-parser/models/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-        return parser
-
-    def get_ner_tags(self):
-        os.environ['CLASSPATH'] = dir+'stanford-ner'
-        return StanfordNERTagger(dir+'stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz').tag(self.tokenized_text)
+from Sentence import Sentence
 
 def generateYesNo(inputStr):
     sent = Sentence(inputStr,0)
@@ -87,7 +55,6 @@ def generateYesNo(inputStr):
         return (2, ans)
     else:
         return (1, ans)
-
 
 def main():
     article = sys.argv[1]
