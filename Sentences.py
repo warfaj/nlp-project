8e39961f9993c5acb91b5927d69e13a0853e122e
xiaowilliam <williamx@andrew.cmu.edu>
2016-11-29 01:24:22 -0500

    Incorporated all questions

--- Sentences.py
+++ Sentences.py
@@ -3,6 +3,7 @@ import re
 from nltk.parse import stanford
 from nltk import word_tokenize, pos_tag
 from nltk.tag.stanford import StanfordNERTagger
+from collections import defaultdict
 
 #dir = '/Users/Warfa/Desktop/nlp-project/'
 dir = ''
@@ -14,17 +15,39 @@ class Sentences:
     def __init__(self, raw_sentences):
         # modify raw_sentences to contain contain ascii characters
         raw_asciis = raw_sentences.strip().decode("ascii","ignore").encode("ascii")
+        self.pronoun = get_default_pronoun(raw_asciis)
         # list of sentences and tokenized sentences, respectively
         self.sentences = re.split('(?<=[.!?])\n+|(?<=[.!?]) +', raw_asciis)
         self.tokenized_sents = [word_tokenize(sent) for sent in self.sentences]
         # number of sentences
         self.size = len(self.sentences)
-        # list of sentences, parse trees, ner_tags, and pos_tags, respectively
+        # list of sentences, parse trees, ner_tags, when_tags, and pos_tags, respectively
         self.parses = get_parses(self.sentences)
         self.ner_tags = get_ner_tags(self.tokenized_sents)
+        self.when_tags = get_when_tags(self.tokenized_sents)
         self.pos_tags = get_pos_tags(self.tokenized_sents)
 
 
+# given a string of raw asciis, returns the most common pronoun
+def get_default_pronoun(raw_asciis):
+    try:
+        tokenized_sent = word_tokenize(raw_asciis)
+        os.environ['CLASSPATH'] = dir+'stanford-ner'
+        ner_tags = StanfordNERTagger(dir+'stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz').tag(tokenized_sent)
+        counts = defaultdict(int)
+        for (word,tag) in ner_tags:
+            if tag == 'PERSON':
+                counts[word] +=1
+        max = None
+        max_count = 0
+        for key in counts:
+            count = counts[key]
+            if count > max_count:
+                max = key
+                max_count = count
+        return max
+    except:
+        return None
 
 # given a list of sentences, returns a list of parse trees
 def get_parses(sentences):
@@ -44,6 +67,11 @@ def get_ner_tags(tokenized_sents):
     os.environ['CLASSPATH'] = dir+'stanford-ner'
     return StanfordNERTagger(dir+'stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz').tag_sents(tokenized_sents)
 
+# given a list of tokenized sentences, returns a list of when_tags
+def get_when_tags(tokenized_sents):
+    os.environ['CLASSPATH'] = dir+'stanford-ner'
+    return StanfordNERTagger(dir+'stanford-ner/classifiers/english.muc.7class.distsim.crf.ser.gz').tag_sents(tokenized_sents)
+
 # given a list of tokenized sentences, returns a list of pos_tags
 def get_pos_tags(tokenized_sents):
     return [pos_tag(sent) for sent in tokenized_sents]
@@ -57,8 +85,9 @@ class Sentence:
         self.tokenized_sent = sents.tokenized_sents[k]
         self.parse = sents.parses[k]
         self.ner_tag = sents.ner_tags[k]
+        self.when_tag = sents.when_tags[k]
         self.pos_tag = sents.pos_tags[k]
-
+        self.pronoun = sents.pronoun
 
 '''
 # example usage
@@ -71,6 +100,8 @@ for k in xrange(sents.size):
     print sent.tokenized_sent
     print sent.parse
     print sent.ner_tag
+    print sent.when_tag
     print sent.pos_tag
+    print sent.pronoun
     print ''
 '''
--- ask.py
+++ ask.py
@@ -4,13 +4,8 @@ import re
 import copy
 
 from Sentences import Sentences, Sentence
-'''
-from nltk import word_tokenize
-from who import whoQuestion
-from what import whatQuestion
-'''
 
-def yesNoHelper(pos_tag):
+def yesNoHelper(pos_tag, pronoun):
     tagged = copy.deepcopy(pos_tag)
     needChange = False
     starts = {'is', 'was', 'are', 'were', 'has', 'have', 'had', 'will', 'would', 'can', 'could', 'shall', 'should'}
@@ -30,8 +25,11 @@ def yesNoHelper(pos_tag):
     
     for i in xrange(len(tagged)-1):
         word = tagged[i+1][0]
-        if i+1 == 1 and tagged[1][1] != 'NNP':
-            word = word.lower()
+        if i+1 == 1:
+            if tagged[1][1] == 'PRP':
+                word = pronoun
+            elif tagged[1][1] != 'NNP':
+                word = word.lower()
         if i+1 == len(tagged)-1:
             if tagged[i+1][0] in {'.', '!'}:
                 word = '?'
@@ -54,43 +52,152 @@ def yesNoHelper(pos_tag):
 
     if not punc:
         return (ans, 4)
-    elif not proper:
-        return (ans, 3)
     elif ',' in ans or 'and' in ans:
+        return (ans, 3)
+    elif not proper:
         return (ans, 2)
     else:
         return (ans, 1)
 
 def yesNoQuestion(sent):
-    return yesNoHelper(sent.pos_tag)
+    return [yesNoHelper(sent.pos_tag, sent.pronoun)]
 
 def whenQuestion(sent):
-    when = sent.when_tag
+    when = copy.deepcopy(sent.when_tag)
     if len(when) <= 2 or when[1][1] not in {'DATE', 'TIME'}:
-        return ('', 10)
+        return []
     idx = 1
     while (when[idx][1] in {'DATE', 'TIME'} or when[idx][0] == ','):
         idx += 1
     ans = 'When did '
-    for i in range(idx, len(when)-1):
-        ans += when[i][0] + ' '
-    ans += '?'
+    score = 2
     if sent.pos_tag[idx][1] in {'NNP', 'DT'}:
-        return (ans, 1)
+        score = 0
+    if sent.pos_tag[idx][1] == 'PRP':
+        ans += sent.pronoun + ' '
+        score = 1
     else:
-        return (ans, 3)
-
+        ans += when[idx][0] + ' '
+    needChange = True
+    for i in range(idx+1, len(when)-1):
+        if needChange:
+            try:
+                ans += en.verb.present(when[i][0]) + ' '
+                needChange = False
+            except:
+                ans += when[i][0] + ' '
+        else:
+            ans += when[i][0] + ' '
+    ans += '?'
+    return [(ans, score)]
 
 def whyQuestion(sent):
     tokens = copy.deepcopy(sent.tokenized_sent)
     cuz_idx = [i for i, w in enumerate(tokens) if w == 'because']
     if not cuz_idx:
-        return ('', 10)
-    yesNoPair = yesNoHelper(sent.pos_tag[:cuz_idx[0]])
+        return []
+    yesNoPair = yesNoHelper(sent.pos_tag[:cuz_idx[0]], sent.pronoun)
     yesNoQues = yesNoPair[0] + ' ?'
     ans = 'Why ' + yesNoQues[0].lower() + yesNoQues[1:]
-    return (ans, 1)
+    return [(ans, 1)]
 
+
+
+
+
+
+def whoQuestion(info):
+    words=info.tokenized_sent[:len(info.tokenized_sent)-1]
+    whoIndices=[]
+    questions=[]
+    for pos in ['NNP','PRP']:
+        for tag in range(len(info.pos_tag)):
+            if info.pos_tag[tag][1]==pos and info.ner_tag[tag][1]=='PERSON':
+                whoIndices+=[tag]
+    for whoIndex in whoIndices:
+        question=''
+        score=10
+        for word in range(len(words)):
+            if word==whoIndex:
+                if word==0:
+                    score-=4
+                    question+='Who '
+                    if len(words)>1 and (info.pos_tag[1][1]=='VBZ' or info.pos_tag[1][1]=='VBD'):
+                        score-=5
+                else:
+                    if word==len(words)-1:
+                        question+='who?'
+                    else:
+                        question+='who '
+            else:
+                if word==len(words)-1:
+                        question+=words[word]+'?'
+                else:
+                    question+=words[word]+' '
+        questions+=[(question,score)]
+    return questions
+
+def whatQuestion(info):
+    words=info.tokenized_sent[:len(info.tokenized_sent)-1]
+    whatIndices=[]
+    for pos in ['NN','NNS']:
+        for tag in range(len(info.pos_tag)):
+            if info.pos_tag[tag][1]==pos:
+                whatIndices+=[tag]
+    questions=[]
+    for whatIndex in whatIndices:
+        question=''
+        score=4
+        if whatIndex!=-1:
+            for word in range(len(words)):
+                if info.pos_tag[word]!='JJ':
+                    if word==whatIndex:
+                        if word==0:
+                            question+='What '
+                        elif word==len(words)-1:
+                            question+='what?'
+                        else:
+                            question+='what '
+                    else:
+                        if word==len(words)-1:
+                            question+=words[word]+'?'
+                        else:
+                            question+=words[word]+' '
+        if word>1:
+            if info.pos_tag[word-1][1] in ['DT','VBZ','VBD']:score-=3
+        ptags=[item[1]for item in info.pos_tag]
+        for pos in ['NNP','PRP','JJ']:
+            if pos in ptags:score+=5
+        for word in ['Thus','thus','This','this','These','these','Those','those','That','that','also','Also']:
+            if word in question:score+=5
+        questions+=[(question,score)]
+    return questions
+
+def whereQuestion(info):
+    words=info.tokenized_sent[:len(info.tokenized_sent)-1]
+    whereIndices=[]
+    for tag in range(len(words)):
+        if info.pos_tag[tag][1]=='LOCATION':
+            whereIndices+=[tag]
+    questions=[]
+    for whereIndex in whereIndices:
+        question=''
+        score=10
+        for word in range(len(words)):
+            if word==whereIndex:
+                if word==0:
+                    question+='Where '
+                elif word==len(words)-1:
+                    question+='where?'
+                else:
+                    question+='where '
+            else:
+                if word==len(words)-1:
+                    question+=words[word]+'?'
+                else:
+                    question+=words[word]+' '
+        questions+=[(question,score)]
+    return questions
 
 
 
@@ -99,18 +206,26 @@ def whyQuestion(sent):
 def main():
     article = sys.argv[1]
     nquestions = sys.argv[2]
-    q = [('What is the article about?',1)]
+    q = [('What is the article about?',3)]
     with open(article) as f:
         raw_sentences = f.read()
         sents = Sentences(raw_sentences)
         for k in xrange(sents.size):
             sent = Sentence(sents, k)
-            #q += whoQuestion(sent)
-            q += whatQuestion(sent)
-            #q += whereQuestion(sent)
+            try: q += yesNoQuestion(sent)
+            except: pass
+            try: q += whenQuestion(sent)
+            except: pass
+            try: q += whyQuestion(sent)
+            except: pass
+            try: q += whoQuestion(sent)
+            except: pass
+            try: q += whatQuestion(sent)
+            except: pass
+            try: q += whereQuestion(sent)
+            except: pass
         q = sorted(q, key=lambda x: x[1])
     for i in xrange(int(nquestions)):
-        print q[i%len(q)]
-#        print q[i][0]
+        print q[i%len(q)][0]
 
 main()
