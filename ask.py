82465b37dae530a1f14746bae9891d77cbfab8b6
xiaowilliam <williamx@andrew.cmu.edu>
2016-11-29 16:18:38 -0500

    Updated where questions

--- ask.py
+++ ask.py
@@ -154,31 +154,26 @@ def whatQuestion(info):
 
 def whereQuestion(info):
     words=info.tokenized_sent[:len(info.tokenized_sent)-1]
-    whereIndices=[]
-    for tag in range(len(words)):
-        if info.pos_tag[tag][1]=='LOCATION':
-            whereIndices+=[tag]
+    values=[]
+    (subject,verb,place,done)=(True,False,False,False)
     questions=[]
-    for whereIndex in whereIndices:
-        question=''
-        score=10
-        for word in range(len(words)):
-            if word==whereIndex:
-                if word==0:
-                    question+='Where '
-                elif word==len(words)-1:
-                    question+='where?'
-                else:
-                    question+='where '
-            else:
-                if word==len(words)-1:
-                    question+=words[word]+'?'
-                else:
-                    question+=words[word]+' '
-        questions+=[(question,score)]
+    for word in range(len(words)):
+        if subject and info.pos_tag[word][1] in ['NN','NNP']:
+            values+=[words[word]]
+            (subject,verb,place,done)=(False,True,False,False)
+        elif verb and info.pos_tag[word][1] in ['VBD','VBZ']:
+            values+=[words[word]]
+            (subject,verb,place,done)=(False,False,True,False)
+        elif place and info.ner_tag[word][1]=='LOCATION':
+            if words[word-1]in['in','at']:
+                values+=[words[word]]
+            elif info.pos_tag[word-1]=='DT':
+                if words[word-2]in ['in','at']:
+                    values+=[words[word]]
+    if len(values)==3:
+        try:questions+=[('Where did '+values[0]+' '+en.verb.present(values[1])+'?',4)]
+        except:questions+=[('Where did '+values[0]+' '+(values[1])+'?',6)]
     return questions
-
-
 
 
 
